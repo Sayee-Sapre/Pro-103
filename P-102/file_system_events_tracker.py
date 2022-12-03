78efe605b412ac7689a1fbc93bf89897973d143c
Sayee-Sapre <73993414+Sayee-Sapre@users.noreply.github.com>
2022-12-03 17:04:22 +0530

    Add files via upload

---
+++ P-102/file_system_events_tracker.py
@@ -0,0 +1,36 @@
+import sys
+import time
+import random
+import os
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/HP/Downloads"
+
+class FileMovementHandle(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"Hey,{event.src_path} has been created!")
+
+    def on_deleted(self, event):
+        print(f"Oops!! Someone deleted {event.src_path}")  
+
+event_handler = FileMovementHandle()
+
+observer = Observer()
+
+observer.schedule(event_handler, from_dir, recursive=True)
+
+observer.start()
+
+try:
+    while True:
+        time.sleep(2)
+        print("running....")
+except KeyboardInterrupt:
+    print("stopped!")
+    observer.stop()
+
+
+
+
